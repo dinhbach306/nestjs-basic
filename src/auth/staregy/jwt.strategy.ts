674d4b17a16da9516982950d85effd2640aaa2bc
DinhB <dinhb@splus-software.com.vn>
2024-10-02 18:09:03 +0700

    add: 'secret key'

--- src/auth/staregy/jwt.strategy.ts
+++ src/auth/staregy/jwt.strategy.ts
@@ -1,18 +1,11 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private configService: ConfigService,
-    private readonly logger = new Logger(JwtStrategy.name),
-  ) {
-    if (!configService.get<string>('JWT_KEY_SECRET')) {
-      logger.error('JWT_KEY_SECRET is not defined');
-      throw new Error('JWT_KEY_SECRET is not defined');
-    }
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
