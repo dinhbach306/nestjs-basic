7b129bf50b5ebb337da1df111da8f1c607bb95a1
DinhB <dinhb@splus-software.com.vn>
2024-10-02 10:45:56 +0700

    refactor: 'cloudbuild with high config'

--- src/auth/staregy/jwt.strategy.ts
+++ src/auth/staregy/jwt.strategy.ts
@@ -6,10 +6,12 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private configService: ConfigService) {
+    const jwtSecretKey = configService.get<string>('JWT_KEY_SECRET');
+
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_KEY_SECRET'),
+      secretOrKey: jwtSecretKey,
     });
   }
 
