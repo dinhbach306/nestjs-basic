dc075de82d913fd077de12d42056e5781e239875
DinhB <dinhb@splus-software.com.vn>
2024-10-02 14:38:44 +0700

    refactor: 'cloudbuild with high config'

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { CompaniesModule } from './companies/companies.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { JwtStrategy } from 'src/auth/staregy/jwt.strategy';
 
 @Module({
   imports: [
@@ -27,6 +28,6 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtStrategy],
 })
 export class AppModule {}
